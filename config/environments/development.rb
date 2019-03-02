fa391e9019821afa665e98572d073062fb50a2a4
小山田賢司 <oyamadakenji@oyamadakenjinoMacBook-ea.local>
2019-03-02 16:41:46 +0900

    heroku再アップの練習

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
 
-  
+  config.action_mailer.default_url_options = { host: "localhost:3000"}
+  config.action_mailer.delivery_method = :letter_opener_web
 
   config.cache_classes = false
 
