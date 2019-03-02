11039e66ca2a9eacdf7708ec91c1b60698393c3b
小山田賢司 <oyamadakenji@oyamadakenjinoMacBook-ea.local>
2019-03-02 17:39:19 +0900

    recommi

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost:3000"}
   config.action_mailer.delivery_method = :letter_opener_web
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.SendGrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   config.cache_classes = false
 
